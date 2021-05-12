b3435d185106169b0e780d64d7f0d99340bd2ea8
IZulz <39718158+IZulz@users.noreply.github.com>
2021-05-12 17:13:58 +0700

    Endless Runner File

---
+++ File/Endless Runner/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Movement")]
+    public float moveAccel;
+    public float maxSpeed;
+
+    [Header("Jump")]
+    public float jumpAccel;
+
+    [Header("Ground Raycast")]
+    public float groundRaycastDistance;
+    public LayerMask groundLayerMask;
+
+    [Header("Scoring")]
+    public ScoreController score;
+    public float scoringRatio;
+    private float lastPositionX;
+
+    [Header("GameOver")]
+    public GameObject gameOverScreen;
+    public float fallPositionY;
+
+    [Header("Camera")]
+    public CameraFollow gameCamera;
+
+    bool isJumping;
+    bool isOnGround;
+
+    Rigidbody2D rigid;
+    Animator anim;
+    PlayerSoundController sound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        sound = GetComponent<PlayerSoundController>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (isOnGround)
+            {
+                isJumping = true;
+                sound.PlayJump();
+            }
+        }
+
+        anim.SetBool("isOnGround" , isOnGround);
+
+        int distancePassed = Mathf.FloorToInt(transform.position.x - lastPositionX);
+        int scoreIncrement = Mathf.FloorToInt(distancePassed / scoringRatio);
+
+        if (scoreIncrement > 0)
+        {
+            score.IncreaseCurrentScore(scoreIncrement);
+            lastPositionX += distancePassed;
+        }
+
+        if (transform.position.y < fallPositionY)
+        {
+            GameOver();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, groundRaycastDistance, groundLayerMask);
+        if (hit)
+        {
+            if(!isOnGround && rigid.velocity.y <= 0)
+            {
+                isOnGround = true;
+            }   
+        }
+        else
+        {
+            isOnGround = false;
+        }
+
+        Vector2 vectorVelocity = rigid.velocity;
+
+        if (isJumping)
+        {
+            vectorVelocity.y += jumpAccel;
+            isJumping = false;
+        }
+
+        vectorVelocity.x = Mathf.Clamp(vectorVelocity.x + moveAccel * Time.deltaTime, 0f, maxSpeed);
+
+        rigid.velocity = vectorVelocity;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Debug.DrawLine(transform.position, transform.position + (Vector3.down * groundRaycastDistance), Color.white);
+    }
+
+    private void GameOver()
+    {
+        score.FinishScoring();
+
+        gameCamera.enabled = false;
+
+        gameOverScreen.SetActive(true);
+
+        this.enabled = false;
+    }
+}
---
+++ File/Endless Runner/Assets/Scripts/TerrainGeneratorController.cs
@@ -0,0 +1,160 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainGeneratorController : MonoBehaviour
+{
+    [Header("Templates")]
+    public List<TerrainTemplateController> terrainTemplates;
+    public float terrainTemplateWidth;
+
+    [Header("Generaor Area")]
+    public Camera gameCamera;
+    public float areaStartOffset;
+    public float areaEndOffset;
+
+    [Header("Force Early Template")]
+    public List<TerrainTemplateController> earlyTerrainTemplates;
+
+    private const float debugLineHeight = 10.0f;
+
+    List<GameObject> spawnedTerrain;
+
+    float lastGereratedPositionX;
+    float lastRemovedPositionX;
+
+    Dictionary<string, List<GameObject>>pool;
+
+
+    private void Start()
+    {
+        pool = new Dictionary<string, List<GameObject>>();
+
+        spawnedTerrain = new List<GameObject>();
+
+        lastGereratedPositionX = GetHorizontalPositionStart();
+        lastRemovedPositionX = lastGereratedPositionX - terrainTemplateWidth;
+
+        foreach (TerrainTemplateController terrain in earlyTerrainTemplates)
+        {
+            GenerateTerrain(lastGereratedPositionX, terrain);
+            lastGereratedPositionX += terrainTemplateWidth;
+        }
+
+        while (lastGereratedPositionX < GetHorizontalPositionEnd())
+        {
+            GenerateTerrain(lastGereratedPositionX);
+            lastGereratedPositionX += terrainTemplateWidth;
+        }
+    }
+
+    private void Update()
+    {
+        while (lastGereratedPositionX < GetHorizontalPositionEnd())
+        {
+            GenerateTerrain(lastGereratedPositionX);
+            lastGereratedPositionX += terrainTemplateWidth;
+        }
+        while(lastRemovedPositionX + terrainTemplateWidth < GetHorizontalPositionStart())
+        {
+            lastRemovedPositionX += terrainTemplateWidth;
+            RemoveTerrain(lastRemovedPositionX);
+        }
+    }
+
+    private GameObject GenerateFromPool(GameObject item, Transform parent)
+    {
+        if (pool.ContainsKey(item.name))
+        {
+            if(pool[item.name].Count > 0)
+            {
+                GameObject newItemFromPool = pool[item.name][0];
+                pool[item.name].Remove(newItemFromPool);
+                newItemFromPool.SetActive(true);
+                return newItemFromPool;
+            }
+        }
+        else
+        {
+            pool.Add(item.name, new List<GameObject>());
+        }
+
+        GameObject newItem = Instantiate(item, parent);
+        newItem.name = item.name;
+        return newItem;
+    }
+
+    private void ReturnToPool(GameObject item)
+    {
+        if (!pool.ContainsKey(item.name))
+        {
+            Debug.LogError("Invalid pool item");
+        }
+
+        pool[item.name].Add(item);
+        item.SetActive(false);
+    }
+
+    private void RemoveTerrain(float posX)
+    {
+        GameObject terrainRemove = null;
+
+        foreach(GameObject item in spawnedTerrain)
+        {
+            if(item.transform.position.x <= posX)
+            {
+                terrainRemove = item;
+                break;
+            }
+        }
+
+        if(terrainRemove != null)
+        {
+            spawnedTerrain.Remove(terrainRemove);
+            //Destroy(terrainRemove);
+            ReturnToPool(terrainRemove);
+        }
+    }
+
+    private void GenerateTerrain(float posX, TerrainTemplateController forceTerrain = null)
+    {
+        //GameObject newTerrain = Instantiate(terrainTemplates[Random.Range(0, terrainTemplates.Count)].gameObject, transform);
+
+        GameObject newTerrain = null;
+
+        if (forceTerrain == null)
+        {
+            newTerrain = GenerateFromPool(terrainTemplates[Random.Range(0, terrainTemplates.Count)].gameObject, transform);
+        }
+        else
+        {
+            newTerrain = GenerateFromPool(forceTerrain.gameObject, transform);
+        }
+
+        newTerrain.transform.position = new Vector2(posX, 0f);
+
+        spawnedTerrain.Add(newTerrain);
+    }
+
+    private float GetHorizontalPositionStart()
+    {
+        return gameCamera.ViewportToWorldPoint(new Vector2(0f, 0f)).x + areaStartOffset;
+    }
+
+    private float GetHorizontalPositionEnd()
+    {
+        return gameCamera.ViewportToWorldPoint(new Vector2(1f, 0f)).x + areaEndOffset;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Vector3 areaStartPosition = transform.position;
+        Vector3 areaEndPosition = transform.position;
+
+        areaStartPosition.x = GetHorizontalPositionStart();
+        areaEndPosition.x = GetHorizontalPositionEnd();
+
+        Debug.DrawLine(areaStartPosition + Vector3.up * debugLineHeight / 2, areaStartPosition + Vector3.down * debugLineHeight / 2, Color.red);
+        Debug.DrawLine(areaEndPosition + Vector3.up * debugLineHeight / 2, areaEndPosition + Vector3.down * debugLineHeight / 2, Color.red);
+    }
+}
